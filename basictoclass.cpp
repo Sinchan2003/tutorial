d0bbfd6c394d3cc220441d9ac0c02ae9279d574c
Sinchan2003 <143471476+Sinchan2003@users.noreply.github.com>
2024-04-06 09:51:54 +0530

    Add files via upload

---
+++ basictoclass.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <conio.h>
+class Time
+{
+	int hrs,min;
+	public:
+            Time(int);
+		void display();
+};
+
+Time :: Time(int t)
+{
+	cout<<"Basic Type to ==> Class Type Conversion..."<<endl;
+	hrs=t/60;
+	min=t%60;
+}
+
+void Time::display()
+{
+	cout<<hrs<< ": Hours(s)" <<endl;
+	cout<<min<< " Minutes" <<endl;
+}
+
+void main()
+{
+	clrscr();
+	
+	int duration;
+	cout<<"Enter time duration in minutes"; 
+    cin>>duration;
+	
+	Time t1=duration;
+        
+	t1.display();
+	getch();
+}
